461ace348cb003dc8754e76dc126fb7c5e96f658
aluguritarun3006 <99875462+aluguritarun3006@users.noreply.github.com>
2022-02-17 16:33:22 +0530

    delete resource

--- tarun_github.tf
+++ tarun_github.tf
@@ -15,14 +15,3 @@ resource "aws_s3_bucket_acl" "example" {
   acl    = "private"
 }
 
-resource "aws_s3_bucket" "b2" {
-  bucket = "my-tf-test-bucket2"
-
-  tags = {
-    Name        = "My bucket tarun"
-    Environment = "Dev"
-  }
-  versioning {
-    enabled = true
-  }
-}
