efd368f00fb3fee0790670752064a58c59f7976b
aluguri-tarun <atarun19@gmail.com>
2022-08-10 18:29:20 +0530

    Update tarun_github.tf

--- tarun_github.tf
+++ tarun_github.tf
@@ -1,8 +1,21 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "unrestricted" {
+  bucket = "examplebuckettftest"
+  #zpc-skip-policy: ZS-AWS-00034:testing
   acl    = "private"
-  tags = {
-    Name        = "My bucket here update"
-    Environment = "Dev"
+  versioning {
+    enabled = true
+    mfa_delete = true
+  }
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
+    }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
   }
 }
